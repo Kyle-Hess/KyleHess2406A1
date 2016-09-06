6639b4e42164930c7f53b6950cdc61926c704cb2
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2016-09-06 13:03:01 +1000

    can create player objects from number input

--- src/main.java
+++ src/main.java
@@ -1,10 +1,36 @@
-
+import java.util.Scanner;
 
 /**
  * Created by Kyle on 15/08/2016.
  */
 public class main {
     public static void main(String[] args) {
-        
+
+        playerInput();
+
+        Scanner keyboard = new Scanner(System.in);
+        String choice2 = keyboard.nextLine();
+        char c = choice2.charAt(0);
+        while (c != 'q'){
+
+
+            System.out.println(choice2);
+
+
+        }
     }
+
+    public static void playerInput() {
+        System.out.println("How Many players? (3 to 5) ");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+
+        if (choice > 3 || choice < 5 ){
+            System.out.println("something");
+        } else {
+            System.out.println("Error");
+        }
+
+    }
+
 }
--- src/players.java
+++ src/players.java
@@ -1,5 +1,36 @@
 /**
  * Created by Kyle on 28/08/2016.
  */
-public class players {
+import java.util.Scanner;
+
+public class players{
+    public static void main(String args[]) {
+
+    System.out.print("How many Payers? :");
+    Scanner input = new Scanner(System.in);
+    int amount = input.nextInt();
+
+    if (amount > 3 || amount < 5 ){
+        System.out.println("something");
+    } else {
+        System.out.println("Error");
+    }
+
+
+    Player[] players = new Player[amount];
+    for(int i = 0; i < amount; i++) {
+        players[i] = new Player();
+    }
+    Player.numberOfPlayers();
+    }
 }
+class Player {
+    private static int count = 0;
+    public Player(){
+    count++;
+    }
+    public static void numberOfPlayers(){
+        System.out.println(count);
+    }
+}
+
